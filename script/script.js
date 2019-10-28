5040722e3296fa885e0e15433ea404914428b2ed
cucuham <cucuham@gmail.com>
2019-10-28 11:58:07 +0900

    포폴페이지 이미지 추가

--- script/script.js
+++ script/script.js
@@ -14,7 +14,6 @@ $(document).ready(function () {
 function about(){
     $("#gnb>li:nth-child(1)").click(function(){
         $("html,body").animate({scrollTop:801});
-        $(".fadetitle>h3").fadeIn(1000);
     });
 }
 function skill(){
@@ -42,24 +41,24 @@ function navbar(){
             $("#i_nav").css("background","none");
         }
         if(scroll>200){
-            $(".fadetitle1>h3").stop().animate({"opacity":1},800);
+            $(".fadetitle1").stop().animate({"opacity":1},800);
         }else{
-            $(".fadetitle1>h3").stop().animate({"opacity":0},500);
+            $(".fadetitle1").stop().animate({"opacity":0},500);
         }
         if(scroll>1000){
-            $(".fadetitle2>h3").stop().animate({"opacity":1},800);
+            $(".fadetitle2").stop().animate({"opacity":1},800);
         }else{
-            $(".fadetitle2>h3").stop().animate({"opacity":0},500);
+            $(".fadetitle2").stop().animate({"opacity":0},500);
         }
         if(scroll>1800){
-            $(".fadetitle3>h3").stop().animate({"opacity":1},800);
+            $(".fadetitle3").stop().animate({"opacity":1},800);
         }else{
-            $(".fadetitle3>h3").stop().animate({"opacity":0},500);
+            $(".fadetitle3").stop().animate({"opacity":0},500);
         }
         if(scroll>2600){
-            $(".fadetitle4>h3").stop().animate({"opacity":1},800);
+            $(".fadetitle4").stop().animate({"opacity":1},800);
         }else{
-            $(".fadetitle4>h3").stop().animate({"opacity":0},500);
+            $(".fadetitle4").stop().animate({"opacity":0},500);
         }
     });
 }
