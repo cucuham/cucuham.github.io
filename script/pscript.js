4efceefd5bc7de48ca9e4a574690ee4c192c5522
cucuham <cucuham@gmail.com>
2019-10-30 16:10:35 +0900

    수정보완

--- script/pscript.js
+++ script/pscript.js
@@ -56,35 +56,23 @@ function navbar(){
         }
         if(scroll>600){
             $(".fadetitle1").stop().animate({"opacity":1},500);
-            $("#gnb>li:nth-child(1)").addClass("whereis");
-            $("#gnb>li:nth-child(1)").siblings().removeClass("whereis");
         }else{
             $(".fadetitle1").stop().animate({"opacity":0},300);
-            $("#gnb>li:nth-child(1)").removeClass("whereis");
         }
         if(scroll>1450){
             $(".fadetitle2").stop().animate({"opacity":1},500);
-            $("#gnb>li:nth-child(2)").addClass("whereis");
-            $("#gnb>li:nth-child(2)").siblings().removeClass("whereis");
         }else{
             $(".fadetitle2").stop().animate({"opacity":0},300);
-            $("#gnb>li:nth-child(2)").removeClass("whereis");
         }
         if(scroll>2300){
             $(".fadetitle3").stop().animate({"opacity":1},500);
-            $("#gnb>li:nth-child(3)").addClass("whereis");
-            $("#gnb>li:nth-child(3)").siblings().removeClass("whereis");
         }else{
             $(".fadetitle3").stop().animate({"opacity":0},300);
-            $("#gnb>li:nth-child(3)").removeClass("whereis");
         }
         if(scroll>3150){
             $(".fadetitle4").stop().animate({"opacity":1},500);
-            $("#gnb>li:nth-child(4)").addClass("whereis");
-            $("#gnb>li:nth-child(4)").siblings().removeClass("whereis");
         }else{
             $(".fadetitle4").stop().animate({"opacity":0},300);
-            $("#gnb>li:nth-child(4)").removeClass("whereis");
         }
     });
 }
