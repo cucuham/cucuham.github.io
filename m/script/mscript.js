81811df19f99c524d74e89c21ea07cde3ba93c3e
cucuham <cucuham@gmail.com>
2019-11-08 06:14:25 +0900

    모바일 텍스트 수정 및 css수정

--- m/script/mscript.js
+++ m/script/mscript.js
@@ -1,30 +1,32 @@
 $(document).ready(function(){
-    fTitle();
+navbar();
 });
-
-
-function fTitle(){
+function navbar(){
     $(window).scroll(function(){
         var scroll=$("html,body").scrollTop();
-        if(scroll>600){
-            $(".fadetitle1").stop().animate({"opacity":1},300);
+        if(scroll>300){
+            $(".fadetitle1").stop().animate({"opacity":1},500);
         }else{
-            $(".fadetitle1").stop().animate({"opacity":0},100);
+            $(".fadetitle1").stop().animate({"opacity":0},300);
         }
-        if(scroll>1450){
-            $(".fadetitle2").stop().animate({"opacity":1},300);
+        if(scroll>1350){
+            $(".fadetitle2").stop().animate({"opacity":1},500);
         }else{
-            $(".fadetitle2").stop().animate({"opacity":0},100);
+            $(".fadetitle2").stop().animate({"opacity":0},300);
         }
-        if(scroll>2300){
-            $(".fadetitle3").stop().animate({"opacity":1},300);
+        if(scroll>2500){
+            $(".fadetitle3").stop().animate({"opacity":1},500);
         }else{
-            $(".fadetitle3").stop().animate({"opacity":0},100);
+            $(".fadetitle3").stop().animate({"opacity":0},300);
         }
-        if(scroll>3150){
-            $(".fadetitle4").stop().animate({"opacity":1},300);
+        if(scroll>4650){
+            $(".fadetitle4").stop().animate({"opacity":1},500);
         }else{
-            $(".fadetitle4").stop().animate({"opacity":0},100);
+            $(".fadetitle4").stop().animate({"opacity":0},300);
         }
     });
 }
+
+setInterval(function(){
+    $(".fa-mouse-pointer").toggle();
+  }, 250);
